dc8acfc1c93516c11a9e72921daed9131675e938
Recyclosaucisse <105792894+Recyclosaucisse@users.noreply.github.com>
2025-12-28 10:59:02 +0100

    Ajout d'une vérification du format d'adresse mail FROM DEST, mise en place du . ATTENTION ! Selon moi, la vérif d'adresse mail ce fait coté serveur et envoie une réponse. Il faut donc : 1 - Comprendre comment on choppe des informations coté Serveur. 2 - Une fois compris, essayer de déplacer la fonction Demander_et_Envoyer (dans clientExemple.py) dans le serverExemple. 3 - On retourne le print "format invalide ...." + le code d'erreur.
    Sinon, renvoyer au client les codes du serveur en mode : 250, on est co etc...

--- client/Cours/ClientExemple.py
+++ client/Cours/ClientExemple.py
@@ -1,17 +1,35 @@
 import socket
+import re
+
+# ici on va vérifier si le format d'adresse mail est correcte ou non ! 
+def demander_et_envoyer(prompt, sock):
+    while True:
+        email = input(prompt)
+        if re.match(r".+@.+\..+", email):
+            sock.sendall(email.encode('utf-8'))
+            break
+        print("Format invalide. Veuillez entrer une adresse du type x@y.z")
+
 if __name__ == '__main__':
     # Etape 1 : création de la socket cliente
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Etape 1 suite : connexion
         s.connect(('localhost', 65432))
+        # lecture clavier d'une chaine
+
+        ## On va rentrer notre adresse mail et on check que ça suit le format x@y.z.
+        ## C'est la meme protocole à suivre que ça soit Email destinataire ou bien Emission.
+        demander_et_envoyer("MAIL FROM : ", s)
+        demander_et_envoyer("MAIL TO : ", s)
+
         while True:
-            # lecture clavier d'une chaine
-            st = input("Tapez une chaine (FIN pour arreter): ")
-            # condition d'arrêt
-            if st == "FIN":
+            st = input("Tapez une chaine (. pour arreter): ")
+            # condition d'arrêt, dans la consigne, c'est un .
+            if st == ".":
                 break
             # Etape 2 : émission de la chaine après encodage
             s.sendall(st.encode('utf-8'))
+            
             # Etape 2 suite : réception de la chaine
             data = s.recv(1024)
             # décodage de la chaine
